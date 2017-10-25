b4203737a75703cc4977e9d3e191f88a98029ab7
evvbmstu <evv.february@gmail.com>
2017-10-25 17:43:08 +0300

    Try some mock.

---
+++ tests.py
@@ -0,0 +1,40 @@
+import unittest
+
+import mock
+
+from getters import auth
+
+
+class ClientTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.api = auth()
+
+    @mock.patch('client.requests.get')
+    def test_get_ok(self, mock_get):
+        """
+        Test getting a 200 OK response from the _get method of MyAPIClient.
+        """
+        # Construct our mock response object, giving it relevant expected
+        # behaviours
+        mock_response = mock.Mock()
+        expected_dict = {
+            "breeds": [
+                "pembroke",
+                "cardigan",
+            ]
+        }
+        mock_response.json.return_value = expected_dict
+
+        # Assign our mock response as the result of our patched function
+        mock_get.return_value = mock_response
+
+        url = 'http://api.corgidata.com/breeds/'
+        response_dict = self.client._get(url=url)
+
+        # Check that our function made the expected internal calls
+        mock_get.assert_called_once_with(url=url)
+        self.assertEqual(1, mock_response.json.call_count)
+
+        # If we want, we can check the contents of the response
+        self.assertEqual(response_dict, expected_dict)
