9f592bff86682e0855785799b106f78964f09d08
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-10-27 01:03:52 +0300

    Some refactor

--- dashapp.py
+++ dashapp.py
@@ -7,53 +7,57 @@ from views import CreatorViews as crtr
 import views
 
 
-def create_dash_app(comm_name="pentestit", debug=True):
-    if debug:
-            print("Just start app.")
-    app = dash.Dash()
-    pb = cm.Community(comm_name)
-    if debug:
-        print("We got info of community")
+class CommunityAnalysisApp:
+    def __init__(self, name="pentestit", debug=True, start=False):
+        self.debug = debug
+        self.community_name = name
+        if start:
+            self.start()
 
-    pb.display()
+    def start(self):
+        if self.debug:
+                print("Just start app.")
+        app = dash.Dash()
+        pb = cm.Community(self.community_name)
+        if self.debug:
+            print("We got info of community")
 
-    if debug:
-        print("Okey, we got some data. Start to visualize...")
-    sex_pie = crtr.pie_chart(pb.sex_dist(), ['#F08080', '#6495ED',  '#B22222'], 'Sex')
+        pb.display()
 
-    funnel = views.likes_funnel(pb)
+        if self.debug:
+            print("Okey, we got some data. Start to visualize...")
+        sex_pie = crtr.pie_chart(pb.sex_dist(), ['#F08080', '#6495ED',  '#B22222'], 'Sex')
 
-    plat_data = pb.platform_dist()
+        funnel = views.likes_funnel(pb)
 
-    platform_pie = crtr.pie_chart(plat_data[0], ['#66CDAA', '#EE5C42', '#1874CD'], 'Platform')
-    system_pie = crtr.pie_chart(plat_data[1], ['#8B8386', '#FFE4C4'], 'System')
+        plat_data = pb.platform_dist()
 
-    fig_sex_age = views.ages_gist(pb)
+        platform_pie = crtr.pie_chart(plat_data[0], ['#66CDAA', '#EE5C42', '#1874CD'], 'Platform')
+        system_pie = crtr.pie_chart(plat_data[1], ['#8B8386', '#FFE4C4'], 'System')
 
-    app.layout = html.Div(children=[
-        html.H1(children='Hello Dash'),
+        fig_sex_age = views.ages_gist(pb)
 
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
+        app.layout = html.Div(children=[
+            html.H1(children='Analisys of {0}'.format(self.community_name)),
 
-        dcc.Graph(id='Sex', figure={'data': sex_pie}),
+            dcc.Graph(id='Sex', figure={'data': sex_pie}),
 
-        dcc.Graph(id='Likes funnel', figure={'data': funnel[0], 'layout': funnel[1]}),
+            dcc.Graph(id='Likes funnel', figure={'data': funnel[0], 'layout': funnel[1]}),
 
-        dcc.Graph(id='Plaform', figure={'data': platform_pie}),
+            dcc.Graph(id='Plaform', figure={'data': platform_pie}),
 
-        dcc.Graph(id='System', figure={'data': system_pie}),
+            dcc.Graph(id='System', figure={'data': system_pie}),
 
-        dcc.Graph(id='Ages', figure={'data': fig_sex_age})
+            dcc.Graph(id='Ages', figure={'data': fig_sex_age})
 
-    ])
+        ])
 
-    if debug:
-        print("We draw this awesome graph, bro. Check it.")
+        if self.debug:
+            print("We draw this awesome graph, bro. Check it.")
 
-    app.run_server(debug=True)
+        app.run_server(debug=True)
 
 
 if __name__ == '__main__':
-    create_dash_app()
+    nya = CommunityAnalysisApp(start=True)
+
