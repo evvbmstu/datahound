c99f7e3821ca715358c8e3be8e9f77f505b5bf33
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2017-10-28 16:02:04 +0300

    Clear views

--- dashapp.py
+++ dashapp.py
@@ -3,8 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 import community as cm
-from views import CreatorViews as crtr
-import views
+import views as vws
 
 
 class CommunityAnalysisApp:
@@ -26,16 +25,28 @@ class CommunityAnalysisApp:
 
         if self.debug:
             print("Okey, we got some data. Start to visualize...")
-        sex_pie = crtr.pie_chart(pb.sex_dist(), ['#F08080', '#6495ED',  '#B22222'], 'Sex')
+        sex_pie = vws.pie_chart(pb.sex_dist(), ['#F08080', '#6495ED',  '#B22222'], 'Sex')
 
-        funnel = views.likes_funnel(pb)
+        funnel = vws.funnel(["Views", "Likes", 'Reposts'], pb.likes_funnel(debug=True))
 
-        plat_data = pb.platform_dist()
+        plat_data, sys_data = pb.platform_dist()
 
-        platform_pie = crtr.pie_chart(plat_data[0], ['#66CDAA', '#EE5C42', '#1874CD'], 'Platform')
-        system_pie = crtr.pie_chart(plat_data[1], ['#8B8386', '#FFE4C4'], 'System')
+        platform_pie = vws.pie_chart(plat_data, ['#66CDAA', '#EE5C42', '#1874CD', '#B22222'], 'Platform')
+        system_pie = vws.pie_chart(sys_data, ['#8B8386', '#FFE4C4'], 'System')
 
-        fig_sex_age = views.ages_gist(pb)
+        ages_female, xbins_female, ages_male, xbins_male, ukn = pb.age_dict()
+
+        gist_female = vws.histogram(ages_female, xbins_female, '#FFD7E9', 'Female')
+        gist_male = vws.histogram(ages_male, xbins_male, '#6495ED', 'Male')
+
+        max_age = max(xbins_female['end'], xbins_male['end'])
+        step = xbins_female['size']
+        labels = ['{0}'.format(a) for a in range(0, max_age + step, step)]
+        values = [a for a in range(0, max_age + step, step)]
+
+        lay = vws.layout('Sex/Age', labels, values, 'Age ({0} Uknown)'.format(ukn), 'Percent')
+
+        fig_sex_age = vws.store_2_fig(gist_female, gist_male, lay)
 
         app.layout = html.Div(children=[
             html.H1(children='Analisys of {0}'.format(self.community_name)),
--- views.py
+++ views.py
@@ -3,56 +3,31 @@ import plotly.graph_objs as go
 import funnelview
 
 
-class CreatorViews:
-    @staticmethod
-    def pie_chart(data, colors, name):
-        return [go.Pie(labels=list(data.keys()), values=list(data.values()),
-                       hoverinfo='label+percent', textinfo='value',
-                       marker=dict(colors=colors), name=name, opacity=0.9)]
-
-    @staticmethod
-    def histogram(data, xbins, colors, name):
-        return go.Histogram(x=data, histnorm='percent', xbins=xbins,
-                            marker=dict(color=colors, ), name=name,
-                            opacity=0.75)
-
-    @staticmethod
-    def _store_2_fig(first, second):
-        pass
+def pie_chart(data, colors, name):
+    return [go.Pie(labels=list(data.keys()), values=list(data.values()),
+                   hoverinfo='label+percent', textinfo='value',
+                   marker=dict(colors=colors), name=name, opacity=0.9)]
 
 
+def histogram(data, xbins, colors, name):
+    return go.Histogram(x=data, histnorm='percent', xbins=xbins,
+                        marker=dict(color=colors, ), name=name,
+                        opacity=0.75)
 
 
-def ages_gist(community):
-    ages_female, xbins_female, ages_male, xbins_male, ukn = community.age_dict()
-    gist_female = go.Histogram(
-                            x=ages_female, histnorm='percent', xbins=xbins_female,
-                            marker=dict(color='#FFD7E9', ), name='Female',
-                            opacity=0.75)
-    gist_male = go.Histogram(
-                            x=ages_male, histnorm='percent', xbins=xbins_male,
-                            marker=dict(color='#6495ED', ), name='Male',
-                            opacity=0.75)
-
-    max_age = max(xbins_female['end'], xbins_male['end'])
-    step = xbins_female['size']
-    layout = go.Layout(
-                        title="Sex/Age",
-                        xaxis=dict(
-                                rangemode='tozero', showticklabels=True,
-                                ticktext=['{0}'.format(a) for a in range(0, max_age + step, step)],
-                                tickvals=[a for a in range(0, max_age + step, step)],
-                                title='Age ({0} Uknown)'.format(ukn)),
-                        yaxis=dict(title='Percent'))
-    return go.Figure(data=[gist_female, gist_male], layout=layout)
+def layout(name, xtext, xvals, xname, yname):
+    return go.Layout(title=name, xaxis=dict(rangemode='tozero', showticklabels=True,
+                                            ticktext=xtext, tickvals=xvals, title=xname),
+                     yaxis=dict(title=yname))
 
 
-def likes_funnel(community):
-    funnel = community.likes_funnel(debug=True)
+def store_2_fig(first, second, layout):
+    return go.Figure(data=[first, second], layout=layout)
 
-    phase = ["Views", "Likes", 'Reposts']
+
+def funnel(phase, data):
     values = []
 
     for each in phase:
-        values.append(funnel[each])
+        values.append(data[each])
     return funnelview.funnel_fig(values, phase)
